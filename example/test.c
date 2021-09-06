9922621d11f9f29a47443ff8f7cb2a02bff27b85
nyaren <78408375+nyaren@users.noreply.github.com>
2021-09-06 11:03:15 +0300

    test.c

---
+++ example/test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "confRead.h"
+
+int
+main(int argc, char **argv)
+{
+	int debug;
+	char* IP;
+	char* password;
+        struct data_holder* data_in_file_ptr = NULL;
+	int data_holder_size = 0;
+
+	takeFileContent(argv[1], &data_in_file_ptr, &data_holder_size);
+
+	debug = strtoInt("debug", data_in_file_ptr, data_holder_size);
+	IP = datatoStr("IP", data_in_file_ptr, data_holder_size);
+	password = datatoStr("Password", data_in_file_ptr, data_holder_size);
+
+	printf("Debug: [%d], Password: [%s], IP:[%s]\n", debug, password, IP);
+
+	free(data_in_file_ptr);
+
+	return 0;
+}
