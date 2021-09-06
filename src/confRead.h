143ef1ad1fba729184676c6c938d4e11ae448763
nyaren <78408375+nyaren@users.noreply.github.com>
2021-09-06 10:59:44 +0300

    Update

---
+++ src/confRead.h
@@ -0,0 +1,43 @@
+#ifndef CONFIGURATION_READER_H
+#define CONFIGURATION_READER_H
+
+/**
+ * struct data_holder - indicates variable names and values
+ *
+ * @name: variable name
+ * @data: variable value
+ */
+struct data_holder
+{
+	char name[128];
+	char data[128];
+};
+
+
+/**
+ * printFileContent - prints the content of the given file.
+ * @file_name: path 
+ */
+int printFileContent(const char* file_name);
+
+/**
+ * takeFileContent - takes variable from file and makes available other functions
+ */
+int takeFileContent(const char* file_name, struct data_holder** data_in_file_ptr, int* data_holder_size);
+
+/**
+ * dataAsInt - converts given data type to integer
+ */
+int dataAsInt(const char* data_name, const struct data_holder* data_in_file_ptr, int data_holder_size);
+
+/**
+ * dataAsStr - converts given data type to string
+ */
+char* dataAsStr(const char* data_name, const struct data_holder* data_in_file_ptr, int data_holder_size);
+
+/**
+ * dataAsFloat - converts given data type to float
+ */
+float dataAsFloat(const char* data_name, const struct data_holder* data_in_file_ptr, int data_holder_size);
+
+#endif
